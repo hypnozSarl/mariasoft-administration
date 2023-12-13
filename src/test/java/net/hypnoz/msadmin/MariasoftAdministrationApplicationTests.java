56b497ecc4e609dd7b7d571818a12cf377ab1eba
hypnozSarl <153780646+hypnozSarl@users.noreply.github.com>
2023-12-14 00:40:45 +0100

    Initial commit

---
+++ src/test/java/net/hypnoz/msadmin/MariasoftAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hypnoz.msadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariasoftAdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
