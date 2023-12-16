1b6dcbee8f3567570d546498721a5a9b79990604
hypnozSarl <153780646+hypnozSarl@users.noreply.github.com>
2023-12-16 15:26:34 +0100

    test unitaire

--- src/main/java/net/hypnoz/msadmin/domain/Structures.java
+++ src/main/java/net/hypnoz/msadmin/domain/Structures.java
@@ -10,9 +10,11 @@ import net.hypnoz.msadmin.enumeration.TypeSocieteEnum;
 import net.hypnoz.msadmin.enumeration.ZoneCommercialEnum;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 
 @Setter
@@ -394,4 +396,19 @@ public class Structures extends AbstractAuditingEntity<Long> implements Serializ
         return strSignature;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Structures that = (Structures) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
