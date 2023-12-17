d7b6cb04ba62190d1dda90380952d6b903f8a6a4
hypnozSarl <153780646+hypnozSarl@users.noreply.github.com>
2023-12-17 22:45:19 +0100

    Refactor test classes and add new Module test
    The GroupesResourceTest and StructureResourceTest have been moved under the web/rest directory for better organization. Additionally, a new test class, ModulesResourceTest, has been added to test the Module service. The package-info.java file is also created in the web directory.

--- src/test/java/net/hypnoz/msadmin/web/rest/ModulesResourceTest.java
+++ src/test/java/net/hypnoz/msadmin/web/rest/ModulesResourceTest.java
@@ -33,9 +33,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
