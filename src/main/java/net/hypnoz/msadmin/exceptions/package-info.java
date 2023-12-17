be2072faed81760bbf22d0b82134ff3860eeb620
hypnozSarl <153780646+hypnozSarl@users.noreply.github.com>
2023-12-17 11:03:49 +0100

    add copyright

--- src/main/java/net/hypnoz/msadmin/exceptions/package-info.java
+++ src/main/java/net/hypnoz/msadmin/exceptions/package-info.java
@@ -17,11 +17,4 @@
  *
  */
 
-package net.hypnoz.msadmin.repository;
-
-
-import net.hypnoz.msadmin.domain.CommunCommunication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunCommunicationRepository extends JpaRepository<CommunCommunication, Long> {
-}
+package net.hypnoz.msadmin.exceptions;
--- src/main/java/net/hypnoz/msadmin/web/rest/errors/DefaultErrorApiResponse.java
+++ src/main/java/net/hypnoz/msadmin/web/rest/errors/DefaultErrorApiResponse.java
@@ -1,3 +1,22 @@
+/*
+ *
+ *  * Mariasoft - Enterprise Management Software
+ *  * Copyright (c) 2023. Hypnoz Technologie. All rights reserved.
+ *  *
+ *  * This software and associated documentation files (the "Software") is the
+ *  * proprietary and confidential information of Hypnoz Technologie and is subject
+ *  * to a license agreement. The Software is protected by international copyright
+ *  * and other intellectual property laws and treaties.
+ *  *
+ *  * No part of this Software may be copied, modified, distributed, or reproduced
+ *  * in any form or by any means without prior written permission from Hypnoz Technologie.
+ *  *
+ *  * ANY USE OF THE SOFTWARE NOT EXPRESSLY PERMITTED BY THE TERMS OF THE LICENSE
+ *  * AGREEMENT IS A VIOLATION OF COPYRIGHT LAW AND MAY RESULT IN SEVERE
+ *  * CIVIL AND CRIMINAL PENALTIES.
+ *
+ */
+
 package net.hypnoz.msadmin.web.rest.errors;
 
 import io.swagger.v3.oas.annotations.media.Content;
